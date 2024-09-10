703843ce90612e279616e14341b131eb4e04c14c
brunobarross <altamiro_bruno@yahoo.com.br>
2024-09-10 19:02:52 -0300

    usando arquiterura MVP

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/presentation/HomePresenter.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/presentation/HomePresenter.kt
@@ -2,6 +2,7 @@ package co.tiagoaguiar.tutorial.jokerappdev.presentation
 
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import co.tiagoaguiar.tutorial.jokerappdev.model.Category
 import co.tiagoaguiar.tutorial.jokerappdev.view.CategoryItem
 import co.tiagoaguiar.tutorial.jokerappdev.view.HomeFragment
@@ -11,18 +12,22 @@ class HomePresenter(private val view: HomeFragment) {
     // VIEW <- PRESENTER
     // PRESENTER -> VIEW
     fun findAllCategory() {
+        view.showProgress()
         fakeRequest()
 
     }
 
-    fun onSuccess(response: List<Category>) {
-        val categories = mutableListOf<CategoryItem>()
-        for (category in response) {
-            categories.add(CategoryItem(category))
-        }
+    fun onComplete() {
+        view.hideProgress()
+    }
 
+    fun onSuccess(response: List<String>) {
+        val categories = response.map { category -> Category(category, 0xFFFF0000) }
         view.showCategories(categories)
+    }
 
+    fun onError(message: String) {
+        view.showFailure(message)
 
     }
 
@@ -30,11 +35,20 @@ class HomePresenter(private val view: HomeFragment) {
     private fun fakeRequest() {
         Handler(Looper.getMainLooper()).postDelayed({
             val response = arrayListOf(
-                Category("Categoria 1", 0xfface6e)
+                "Categoria 1"
             )
             //aqui a lista já está pronta
             onSuccess(response)
+//            onError(("FALHA NA CONEXÇAO. TENTA NOVAMENTE!!!"))
+            onComplete()
         }, 2000)
     }
 
 }
+
+
+// 1, CICLO DE VIDA DO FRAGMENT FAZ A AÇÃO (CHAMAR O PRESENTER PARA BUSCAR AS CATEGORIAS
+// 2. O PRESENTER PEDE A LISTA DE CAT. NO MODEL
+// 3. O MODEL DEVOLVE UMA LISTA List<String>
+// 4. O PRESENTER FORMATA ESSA LISTA QUE É UMA STRING EM CATEGORY(MODEL)
+// 5. O VIEW PEGA A LISTA DE List<Category> E CONVERTE PARA O List<CategoryItem>
--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/HomeFragment.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/HomeFragment.kt
@@ -1,9 +1,12 @@
 package co.tiagoaguiar.tutorial.jokerappdev.view
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +18,7 @@ import com.xwray.groupie.GroupieAdapter
 class HomeFragment : Fragment() {
     private lateinit var presenter: HomePresenter
     private val adapter = GroupieAdapter()
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +37,30 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.rv_home)
-
         recyclerView.layoutManager = LinearLayoutManager(requireContext()) //busca o contexto
+        progressBar = view.findViewById(R.id.home_progress_bar)
         presenter.findAllCategory()
         recyclerView.adapter = adapter
-
         adapter.notifyDataSetChanged()
     }
 
-    fun showCategories(categories: List<CategoryItem>) {
+    fun showCategories(response: List<Category>) {
+        val categories = response.map { category -> CategoryItem(category) }
         adapter.addAll(categories)
         adapter.notifyDataSetChanged()
+
+    }
+
+    fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun hideProgress() {
+        progressBar.visibility = View.GONE
+    }
+
+    fun showFailure(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
 
     }
 }
